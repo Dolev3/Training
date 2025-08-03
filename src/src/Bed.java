67ff9a73df6d0a163b390c94fea79a8f61589c32
Yahli <dolev.yahli@gmail.com>
2025-08-03 16:37:52 +0300

    Until exercise 5

--- src/src/Bed.java
+++ src/src/Bed.java
@@ -2,16 +2,22 @@ public class Bed {
     private double  height;
     private  int pillows;
     private String SheetsColor;
+    private int price;
 
-    public Bed (double height, int pillows, String SheetsColor) {
+    public Bed (double height, int pillows, String SheetsColor, int price) {
         this.height = height;
         this.pillows = pillows;
         this.SheetsColor = SheetsColor;
+        this.price = price;
     }
     public Bed (double height,  String SheetsColor) {
         this.height = height;
         this.SheetsColor = SheetsColor;
         this.pillows = 1;
+        this.price = 100;
+    }
+    public int getPrice() {
+        return price;
     }
     public double getHeight() {
         return height;
@@ -21,5 +27,8 @@ public class Bed {
     }
     public String getSheetsColor() {
         return SheetsColor;
+    }
+    public void setSheetsColor(String SheetsColor) {
+        this.SheetsColor = SheetsColor;
     }
 }
