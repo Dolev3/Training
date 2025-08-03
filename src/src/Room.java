94850d06ac9e31790f038972e7cfa59f4768aad0
Yahli <dolev.yahli@gmail.com>
2025-08-03 17:16:15 +0300

    Until exercise 13

---
+++ src/src/Room.java
@@ -0,0 +1,22 @@
+public class Room {
+    private Closet closet;
+    private Bed bed;
+
+    public Room (Closet closet, Bed bed) {
+        this.closet = closet;
+        this.bed = bed;
+    }
+
+    public Room (Bed bed, Shirt[] shirts) {
+        this.bed = bed;
+        this.closet = new Closet(shirts);
+    }
+    public Closet getCloset() {
+        return closet;
+    }
+
+    public Bed getBed() {
+        return bed;
+    }
+
+}
