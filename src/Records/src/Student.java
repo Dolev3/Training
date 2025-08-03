6b270e67b388f1155393c983e58fa81999345698
Yahli <dolev.yahli@gmail.com>
2025-08-03 17:47:19 +0300

    Exercise 5

--- src/Records/src/Student.java
+++ src/Records/src/Student.java
@@ -2,6 +2,10 @@
     public String toString() {
         return name+" is a student in the "+ Class+ "th grade and class number "+ClassNum;
     }
+
+    public Student(String name, int ClassNum) {
+        this(name,9,ClassNum);
+    }
     public boolean isHighschool(int Class) {
         if (Class >= 10)
             return true;
