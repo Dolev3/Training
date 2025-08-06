dddf30c981150876c7d8a93f0edf3e7352897cf7
Yahli <dolev.yahli@gmail.com>
2025-08-06 17:11:34 +0300

    Update 6

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,15 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ex_15();
+    }
+    public static void ex_15() {
+        Shirt [] shirts = new Shirt[10];
+        Closet closet = new Closet(shirts);
+        Room r1 = new Room(closet, new Bed(8,2,"Blue",700) );
+        r1.set
     }
 }
--- src/src/YahliConstants.java
+++ src/src/YahliConstants.java
@@ -1,3 +1,4 @@
 public class YahliConstants {
-
+    private static final int SHIRT_SIZE = 54;
+    private static final String FAVORITE_COLOR = "Blue";
 }
