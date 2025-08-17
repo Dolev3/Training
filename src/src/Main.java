081080fb1cda61f1dc77a61a1ee167570c64cad8
Yahli <dolev.yahli@gmail.com>
2025-08-17 16:38:01 +0300

    Update 3

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,26 +1,32 @@
 public class Main {
     public static void main(String[] args) {
         ex_15();
+        ex_20();
     }
+
+    public static void ex_20() {
+        System.out.println(Util.tolerance(YahliConstants.HIGHEST_PRICE_FOR_BED,Bed.getDefaultBedPrice(),YahliConstants.TOLERANCE_FOR_PRICE_OF_BED));
+    }
+
     public static void ex_15() {
         Shirt[] shirts = new Shirt[10];
         Closet closet = new Closet(shirts);
         Bed bed = new Bed(10, "Black");
         Room r1 = new Room(closet, bed);
-        bed.setSheetsColor(YahliConstants.getFavoriteColor());
+        bed.setSheetsColor(YahliConstants.FAVORITE_COLOR);
 
-        Shirt[] shirtsBySize = closet.getShirtsBySize(shirts, YahliConstants.getShirtSize());
+        Shirt[] shirtsBySize = closet.getShirtsBySize(shirts, YahliConstants.SHIRT_SIZE);
 
         int count = 0;
         int index = 0;
         for (int i = 0; i < shirtsBySize.length; i++) {
-        if (shirtsBySize[i].getColor().equals(YahliConstants.getFavoriteColor())) {
+        if (shirtsBySize[i].getColor().equals(YahliConstants.FAVORITE_COLOR)) {
             count++;
         }
     }
         Shirt[] shirtsBySizeAndColor = new Shirt[count];
         for (int i = 0; i < shirtsBySizeAndColor.length; i++) {
-            if (shirtsBySize[i].getColor().equals(YahliConstants.getFavoriteColor())) {
+            if (shirtsBySize[i].getColor().equals(YahliConstants.FAVORITE_COLOR)) {
                 shirtsBySizeAndColor[index] = shirtsBySize[i];
             }
         }
--- src/src/Main2.java
+++
@@ -1,5 +0,0 @@
-public class Main2  {
-    public static void main(String[] args) {
-        System.out.println(Util.tolerance(YahliConstants.HIGHEST_PRICE_FOR_BED,Bed.getDefaultBedPrice(),YahliConstants.TOLERANCE_FOR_PRICE_OF_BED));
-    }
-}
