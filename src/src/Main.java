9f4115322b53638e5ef999d20a2dbb31a81c357b
Yahli <dolev.yahli@gmail.com>
2025-08-20 21:03:59 +0300

    I hate this shit

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            ex1(new ArrayList<>());
         }
+
+        public static void ex1(List <Integer> untilQ) {
+
+        String  num = sc.next();
+
+        while (!Objects.equals(num, "q")) {
+            num = sc.next();
+            try {
+                untilQ.add(Integer.parseInt(num));
+            } catch (NumberFormatException e) {
+
+            }
+          }
+        }
+
     }
-}
